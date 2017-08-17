5eef7fbe5c7127dcceba3fd72de0c5d61afa8769
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-17 09:29:18 -0700

    making changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var handleMainNav = function() {
+  $('nav ul').on('click', 'li', function() {
+    $('.tab-content').hide();
+    $('.tab-content[id = "' + $(this).attr('data-content') + '"]').show();
+  });
+  $('.main-nav .tab:first').click();
+};
+
+
+$(function() {
+  handleMainNav();
+});
