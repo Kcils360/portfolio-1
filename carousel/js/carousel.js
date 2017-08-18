fdcfcb519efaea762ad5d7453d0b24c760173b3a
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-18 01:33:24 -0700

    working on the button functionality

--- carousel/js/carousel.js
+++ carousel/js/carousel.js
@@ -7,20 +7,33 @@ function Carousel(src, alt) {
   this.alt = alt;
 }
 
-var $carousel = $('.carousel img');
-
 Carousel.getImages = function() {
-  $.each($carousel, function() {
+
+  $.each($('.carousel img'), function() {
     var $src = $(this).attr('src');
     var $alt = $(this).attr('alt');
     carousel.push(new Carousel($src, $alt));
   });
 };
 
-Carousel.toHTML = function() {
 
+
+Carousel.toHTML = function() {
+  var $lfChevron = `<div class="ca-button ca-left">&#12296;</div>`;
+  var $rtChevron = `<div class="ca-button ca-right">&#12297;</div>`;
+
+  $('.carousel').append($lfChevron, $rtChevron);
+};
+
+Carousel.navigation = function() {
+
+  $('.carousel').on('click', '.ca-button', function() {
+    console.log($(this));
+  });
 };
 
 $(function() {
   Carousel.getImages();
+  Carousel.navigation();
+  Carousel.toHTML();
 });
