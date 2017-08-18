4bb2b702318ac44589312a3b810dc9667fa445b1
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-18 15:41:58 -0700

    carousel functioning

--- carousel/js/carousel.js
+++ carousel/js/carousel.js
@@ -8,32 +8,47 @@ function Carousel(src, alt) {
 }
 
 Carousel.getImages = function() {
-
   $.each($('.carousel img'), function() {
     var $src = $(this).attr('src');
-    var $alt = $(this).attr('alt');
+    var $alt = '';
+    if (!$(this).attr('alt')) {
+      $alt = $(this).attr('alt');
+    }
+
     carousel.push(new Carousel($src, $alt));
+    $(this).remove();
   });
 };
 
-
-
 Carousel.toHTML = function() {
+  var $imgCont = `<img src="img/img_1.jpg" alt="Adorable Tiger">`;
   var $lfChevron = `<div class="ca-button ca-left">&#12296;</div>`;
   var $rtChevron = `<div class="ca-button ca-right">&#12297;</div>`;
 
-  $('.carousel').append($lfChevron, $rtChevron);
+  $('.carousel').append($imgCont, $lfChevron, $rtChevron);
+  $('.carousel img').show();
 };
 
 Carousel.navigation = function() {
-  // var prevIndex = 0;
   var currIndex = 0;
   $('.carousel').on('click', '.ca-button', function() {
-
-    currIndex++;
+    var $img = $('.carousel img');
+    if ($(this).hasClass('ca-left')) {
+      console.log('left');
+      currIndex--;
+      if (currIndex < 0) {
+        currIndex = carousel.length - 1;
+      }
+    }
+    if ($(this).hasClass('ca-right')) {
+      console.log('right');
+      currIndex++;
+      if (currIndex >= carousel.length) {
+        currIndex = 0;
+      }
+    }
     console.log(currIndex);
-
-
+    $img.attr('src', carousel[currIndex].src).attr('alt', carousel[currIndex].alt);
   });
 };
 
